d90fe7aa8b001d4ac16beebe7a96630f9186541e
rowanmurdock <142937049+rowanmurdock@users.noreply.github.com>
2025-05-06 22:11:53 -0600

    added ocean tile and tracking for all four wheels

--- Main.gd
+++ Main.gd
@@ -2,19 +2,34 @@ extends Node2D
 
 @onready var tilemap = $TileMap
 
-func _ready():
-	print("in ready")
-	print("running")
-	randomize()  
-	var width = 50
-	var height = 30
-	var tile_ids = [1, 2, 3, 4, 5, 6, 7]
 
+func _ready():
+	randomize()  
+	var width = 1000
+	var height = 500
+	var tile_ids = [1, 2, 3, 4, 5, 6, 7]
+	var ocean_tile_id = 0
+	var border_tile_ids = [0, 1]
+	var border_width = 7
 
 	for x in range(width):
 		for y in range(height):
-			var rand_tile = tile_ids[randi() % tile_ids.size()]
-			tilemap.set_cell(0, Vector2i(x, y), rand_tile, Vector2i(0, 0))
+			var is_ocean = (
+				x < border_width or x >= width - border_width or
+				y < border_width or y >= height - border_width
+			)
+			var is_border = (x == 993 or x == 7 or y == 493 or y == 7)
+
+			if is_ocean:
+				tilemap.set_cell(0, Vector2i(x, y), ocean_tile_id, Vector2i(0, 0))
+			elif is_border:
+				var rand_tile = border_tile_ids[randi() % border_tile_ids.size()]
+				tilemap.set_cell(0, Vector2i(x, y), rand_tile, Vector2i(0, 0))
+			else:
+				var rand_tile = tile_ids[randi() % tile_ids.size()]
+				tilemap.set_cell(0, Vector2i(x, y), rand_tile, Vector2i(0, 0))
+				
+
 
 
 
--- rover.gd
+++ rover.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 @export var speed = 90
 @export var rotation_speed = 3.5
 @export var trail_length = 5000
-@export var right_wheel_offset = Vector2(-8, 6) 
-@export var left_wheel_offset = Vector2(-8, -6)
+@export var right_wheel_offset = Vector2(5, 6) 
+@export var left_wheel_offset = Vector2(5, -7)
+@export var right_bot_wheel_offset = Vector2(-8, 6) 
+@export var left_bot_wheel_offset = Vector2(-8, -7)
 
 @onready var right_trail: Line2D = $"../RightTrailLine"
 @onready var left_trail: Line2D = $"../LeftTrailLine"
@@ -27,6 +29,18 @@ func move_anim():
 		$AnimatedSprite2D.play("move")
 	else:
 		$AnimatedSprite2D.play("default")
+		
+
+
+
+#func fade_trail():
+	#var opacity = 1
+	#while opacity>0:
+		#right_trail.default_color = Color(0.423, 0.219, 0.015, opacity)
+		#opacity-= 0.01
+		#print(opacity)
+		#await get_tree().create_timer(0.01).timeout
+		#
 
 func _physics_process(delta):
 	get_input()
@@ -37,8 +51,12 @@ func _physics_process(delta):
 	if velocity.length() > 0 or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
 		var right_wheel_pos = to_global(right_wheel_offset)
 		var left_wheel_pos = to_global(left_wheel_offset)
+		var right_bot_wheel_pos = to_global(right_bot_wheel_offset)
+		var left_bot_wheel_pos = to_global(left_bot_wheel_offset)
 		right_trail.add_point(right_wheel_pos)
 		left_trail.add_point(left_wheel_pos)
+		right_trail.add_point(right_bot_wheel_pos)
+		left_trail.add_point(left_bot_wheel_pos)
 		
 		
 		if right_trail.get_point_count() > trail_length:
@@ -46,6 +64,5 @@ func _physics_process(delta):
 		if left_trail.get_point_count() > trail_length:
 			left_trail.remove_point(0)
 		
-		
-	
 	move_anim()
+	#fade_trail()
