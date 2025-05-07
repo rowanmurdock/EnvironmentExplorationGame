d453a057c7b611fb28694b49a98664ce51f04617
rowanmurdock <142937049+rowanmurdock@users.noreply.github.com>
2025-05-06 22:47:30 -0600

    created beach sprite and placed beach tiles around ocean tiles

--- Main.gd
+++ Main.gd
@@ -9,7 +9,7 @@ func _ready():
 	var height = 500
 	var tile_ids = [1, 2, 3, 4, 5, 6, 7]
 	var ocean_tile_id = 0
-	var border_tile_ids = [0, 1]
+	var border_tile_ids = [0, 8]
 	var border_width = 7
 
 	for x in range(width):
@@ -18,7 +18,7 @@ func _ready():
 				x < border_width or x >= width - border_width or
 				y < border_width or y >= height - border_width
 			)
-			var is_border = (x == 993 or x == 7 or y == 493 or y == 7)
+			var is_border = ((x <= 993 and x > 990) or (x >= 7 and x < 10) or (y <= 493 and y > 490) or (y >= 7 and y < 10))
 
 			if is_ocean:
 				tilemap.set_cell(0, Vector2i(x, y), ocean_tile_id, Vector2i(0, 0))
